df5b7e06cb24e9f37424a50fe647c309ef233cc9
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-08-03 12:07:56 -0300

    created client folder and scaffolded list

---
+++ db/migrate/20180803145346_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lists do |t|
+      t.string :title
+      t.text :description
+      t.integer :upvotes
+
+      t.timestamps
+    end
+  end
+end
