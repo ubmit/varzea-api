3eb36a7b51a114ca46804eda9c73621da76438cc
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-08-18 11:11:10 -0300

    added heroku domain to origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins '*'
+    origins 'https://rn-rails-prototype.herokuapp.com/'
 
     resource '*',
       headers: :any,
