95ca6a6927cd21547021555d0b7b9d10fe81e4a7
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-08-18 11:44:38 -0300

    initial changes for deployment to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 List.create(title:"Kyuu", description:"A 65% keyboard")
 List.create(title:"5 Degree Frosted", description:"60% case made of acrylic")
+List.create(title:"Vintage Blacks", description:"One of the smoothest linear switches")
+List.create(title:"Samuel PX", description:"The nerdiest brazilian guy")
